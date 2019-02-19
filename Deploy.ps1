24c64d1e45ce85845280fa6967f566fbdbfe724b
Tautvydas <tautsjasiunsas@gmail.com>
2019-02-19 11:38:41 +0200

    deploy

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,8 +1,12 @@
 $ErrorActionPreference = "Stop"
 
-$serviceFolder = "C:\Services\Tucio"
-$serviceName = "TucioBarcodeScanner"
+$serviceFolder = "C:\Services\Beno"
+$serviceName = "BenoBarcodeScanner"
 $artifactsFolder = "package\*"
+
+If((Test-Path $artifactsFolder) -ne $true) {
+    New-Item $artifactsFolder      
+}
 
 Copy-Item $artifactsFolder $serviceFolder
 New-Service -Name $serviceName -BinaryPathName "$serviceFolder\BarcodeScanner.dll"
