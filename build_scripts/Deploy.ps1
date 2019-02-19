3ed120fcd95a2303fe091217e10870bf3f2210d9
Tautvydas <tautsjasiunsas@gmail.com>
2019-02-19 13:02:21 +0200

    benas

--- build_scripts/Deploy.ps1
+++ build_scripts/Deploy.ps1
@@ -8,6 +8,7 @@ Remove-Item $serviceFolder -Recurse -ErrorAction Ignore
 Copy-Item $artifactsFolder -Destination $serviceFolder -Recurse
 
 Stop-Service -Name $serviceName -ErrorAction Ignore
+Start-Sleep -Seconds 5
 Invoke-Expression "sc.exe delete $serviceName"
 New-Service -Name $serviceName -BinaryPathName "$serviceFolder\BarcodeScanner.exe"
 
