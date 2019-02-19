943b38a724beded41a6a88ac1cf25b137a647433
Tautvydas <tautsjasiunsas@gmail.com>
2019-02-19 13:27:09 +0200

    geras build

--- build_scripts/Build.ps1
+++ build_scripts/Build.ps1
@@ -1,3 +1,6 @@
 $ErrorActionPreference = "Stop"
+
 Invoke-Expression "dotnet build"
-Invoke-Expression "dotnet publish BarcodeScanner -o artifacts"
+Invoke-Expression "dotnet test BarcodeScanner.Tests"
+
+Invoke-Expression "dotnet publish BarcodeScanner -o artifacts -r 'win7-x64' --self-contained"
